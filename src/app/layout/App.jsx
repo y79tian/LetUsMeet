d36f8ba6956359769857433e0a809adb6615cddf
Yang Tian <yang.tian@fintelics.com>
2022-07-11 14:36:07 -0400

    scafford of dashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen} />
+      </Container>
+    </>
   );
 }
 
